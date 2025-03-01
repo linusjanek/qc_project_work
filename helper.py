0682fc1dcbe6ba3b225e6623aeeca2aced6ec639
lkoester <linus@koester-im-web.de>
2025-03-01 13:45:52 +0100

    everthing is fucked but also kinda progress

--- helper.py
+++ helper.py
@@ -1,22 +1,21 @@
 import numpy as np
 import random
+# EVERYTHING IN RADIANS
+AzEl = (float, float)
+VectorFloat = (float, float, float)
+QuantumState = (complex, complex)
 
 # return the rotation angle
-def cost_function(start_azel_deg, end_azel_deg):
-    # convert to radians
-    start_azel = [deg * np.pi / 180 for deg in start_azel_deg]
-    end_azel = [deg * np.pi / 180 for deg in end_azel_deg]
+def cost_function(start_azel: AzEl, end_azel: AzEl) -> float:
     return np.arccos(np.sin(start_azel[1]) * np.sin(end_azel[1]) + np.cos(start_azel[1]) * np.cos(end_azel[1]) * np.cos(end_azel[0] - start_azel[0]))
 
-def vector_from_azel(azel: list[int]) -> list[int]:
-    # convert to radians
-    azel = [deg * np.pi / 180 for deg in azel]
+def vector_from_azel(azel: AzEl) -> VectorFloat:
     return [np.sin(azel[0]) * np.cos(azel[1]), np.cos(azel[0]) * np.cos(azel[1]), np.sin(azel[1])]
 
-def rnd_points(n: int) -> list[list[int]]:
-    return [[random.randint(0, 360), random.randint(0, 180)] for i in range(n)]
+def rnd_points(n: int) -> list[AzEl]:
+    return [[random.uniform(0, 2*np.pi), random.uniform(0, np.pi)] for i in range(n)]
 
-def route_cost(route: list[list[int]]):
+def route_cost(route: list[AzEl]) -> float:
     total_cost = 0
     n = len(route)
     for i in range(n):
@@ -24,3 +23,8 @@ def route_cost(route: list[list[int]]):
         next_azel = route[(i + 1) % n] # Wrap around to the start of the route
         total_cost += cost_function(current_azel, next_azel)
     return total_cost
+
+def angle_to_bloch(phi: float, theta: float) -> QuantumState:
+    zero_factor = np.cos(theta/2)
+    one_factor = np.exp(1j * phi/2) * np.sin(theta/2)
+    return (zero_factor, one_factor)
--- quantum.py
+++ quantum.py
@@ -1,5 +1,20 @@
 import numpy as np
 from helper import cost_function
+from helper import angle_to_bloch, rnd_points, QuantumState
+
+class BlochSphere:
+    def __init__(self, azel):
+        # Matrix representation of the Bloch Sphere via indices <P_ij>
+        # see equation (4) in paper
+        self.P = np.ndarray((len(azel), len(azel)), dtype=QuantumState)
+        n = len(azel)
+        print(self.P)
+        for i in range(n):
+            for j in range(n):
+                print(i,j)
+                print(self.P[i][j])
+                print(angle_to_bloch(i*2*np.pi/n, cost_function(azel[i], azel[j])))
+                self.P[i][j] = angle_to_bloch(i*2*np.pi/n, cost_function(azel[i], azel[j]))
 
 def cost_matrix(azel: list[list[int]]):
     m = np.zeros(shape=(len(azel), len(azel)))
@@ -21,3 +36,5 @@ def epsilon_matrix(azel: list[list[int]]) -> np.ndarray:
 
 def quantum(azels):
     return (0,0)
+
+blochsphere = BlochSphere(rnd_points(5))
